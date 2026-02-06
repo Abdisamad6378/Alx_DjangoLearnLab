6d048c4c953f8babdb959d3fffc016252d7b165d
Abdisamad6378 <abdisamadhussein77@gmail.com>
2026-02-06 11:29:05 +0300

    change relationship_app

--- Introduction_to_Django/LibraryProject/bookshelf/update.md
+++
@@ -1,2 +0,0 @@
-from bookshelf.models import Book book = Book.objects.get(pk=1), book.title="Nineteen Eighty-Four", book.save()
-#Nineteen Eighty-Four
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from models import Book
 # Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    context = {'list_books,:books'}
+    return render(request, 'books/list_books.html' , context)
