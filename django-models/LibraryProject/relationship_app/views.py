af439fd42c7e3d955e52104138b2184683ccedb9
Abdisamad6378 <abdisamadhussein77@gmail.com>
2026-02-06 11:51:31 +0300

    change relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 from models import Book
+from .models import library
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
     context = {'list_books,:authors'}
     return render(request, 'relationship_app/list_books.html' , context)
+
+class libraryDeltaview(DetailView):
+    model = library
+    template_name = 'books/library_detail.html'
+    
