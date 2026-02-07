73a254936bd0da769a3ae5b20fc768b3c6bd78e4
Abdisamad6378 <abdisamadhussein6378@gmail.com>
2026-02-07 12:12:38 +0300

    change relationship_app.

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,33 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Author(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
     
     def __str__(self):
         return self.name
-    
+
 class Book(models.Model):
-    title = models.CharField(max_length=30)
-    author = models.ForeignKey(
-        Author, 
-        
-        on_delete=models.CASCADE,
-        related_name='author'
-    )
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     
+    def __str__(self):
+        return self.title
 
 class Library(models.Model):
-    title = models.CharField(max_length=40)
-    book = models.ManyToManyField(
-        Book,
-        related_name = 'LIbrary'
-    )
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='library')
     
-    class Librarian(models.Model):
-        name = models.CharField(max_length=30)
-        Library= models.OneToOneField(
-            library,
-            realted_name = 'librarian'
-        )
-        
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+ROLE_CHOICES = (
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Link to Django's built-in User model
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f'{self.user.username} ({self.role})'
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
