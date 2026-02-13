f7a9a7333c50614c0322dd7b1d9db3fb8d71065b
Abdisamad6378 <abdisamadhussein6378@gmail.com>
2026-02-14 00:20:57 +0300

    changes in advanced api project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, filters, status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated  # ← THIS LINE MUST BE EXACT
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated  # ← THIS EXACT LINE
 from django_filters import rest_framework as django_filters
 from .models import Book
 from .serializers import BookSerializer
